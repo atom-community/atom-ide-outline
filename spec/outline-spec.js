55784037bfa60a746c9f9bf03d4f4f02b2ca5805
Belar <belar@belardesign.com>
2020-03-17 20:28:50 +0100

    feat: add outline update on save

--- spec/outline-spec.js
+++ spec/outline-spec.js
@@ -1,18 +1,29 @@
 "use babel";
 
+import path from "path";
+
+import OutlinePackage from "../lib/main";
+
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe("Outline", () => {
-  let workspaceElement;
+  let workspaceElement, activeEditor;
 
   beforeEach(() => {
+    const mockFilePath = path.join(__dirname, "mockFile");
+
     workspaceElement = atom.views.getView(atom.workspace);
     jasmine.attachToDOM(workspaceElement);
 
+    waitsForPromise(() => atom.workspace.open(mockFilePath));
     waitsForPromise(() => atom.packages.activatePackage("atom-ide-outline"));
+
+    runs(() => {
+      activeEditor = atom.workspace.getActiveTextEditor();
+    });
   });
 
   it("adds toggle command", function() {
@@ -35,5 +46,15 @@ describe("Outline", () => {
     atom.commands.dispatch(workspaceElement, "outline:toggle");
 
     expect(workspaceElement.querySelector(".outline-view")).not.toExist();
+  });
+
+  it("triggers outline generation for active editor on save", () => {
+    spyOn(OutlinePackage, "getOutline");
+
+    waitsForPromise(() => activeEditor.save());
+
+    runs(() => {
+      expect(OutlinePackage.getOutline).toHaveBeenCalled();
+    });
   });
 });
