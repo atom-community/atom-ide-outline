580cf6dbd493a9c3d3d726800fd1463c8b2cfde2
Alejandro Bar Acedo <abaracedo@gmail.com>
2020-07-11 17:43:00 +0200

    remove package rollup-plugin-coffee-scrip

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 // import typescript from "@rollup/plugin-typescript";
-// import coffeescript from 'rollup-plugin-coffee-script';
 import babel from "rollup-plugin-babel";
 // import json from "@rollup/plugin-json"
 import { terser } from "rollup-plugin-terser";
