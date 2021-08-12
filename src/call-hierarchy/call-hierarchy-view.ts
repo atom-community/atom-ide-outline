1359bafad09ae61035c3b67a75cf3368bc3aa735
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-08-12 10:32:08 +0900

    chore: add line breaks

--- src/call-hierarchy/call-hierarchy-view.ts
+++ src/call-hierarchy/call-hierarchy-view.ts
@@ -13,15 +13,21 @@ type statusKey = keyof typeof statuses
 /** HTMLElement for the call-hierarchy tab */
 export class CallHierarchyView extends HTMLElement {
   #subscriptions = new CompositeDisposable()
+  
   /** Subscription to observe editor cursor movement */
   #editorSubscriptions: Disposable | undefined
+
   #providerRegistry: ProviderRegistry<CallHierarchyProvider>
+
   /** Element for outputting results */
   #outputElement: HTMLDivElement
+
   /** Whether to display incoming or outgoing */
   #currentType: CallHierarchyType
+
   /** Time to debounce the timing of display updates */
   #debounceWaitTime = 300
+
   /**
    * Status of currently displayed content
    *
@@ -31,12 +37,15 @@ export class CallHierarchyView extends HTMLElement {
    * - NoResult: provider returns null or empty array (cursor is above something other than function)
    */
   #status: statusKey | "valid" | undefined
+
   /** Whether the tab has already been closed */
   destroyed = false
+
   /** Needed for Atom */
   getTitle = () => "Call Hierarchy"
   /** Needed for Atom */
   getIconName = () => "link"
+
   /** Determine the type of data to display */
   static getStatus(data: CallHierarchy<CallHierarchyType> | statusKey | null | undefined): statusKey | "valid" {
     if (typeof data === "string") {
@@ -47,6 +56,7 @@ export class CallHierarchyView extends HTMLElement {
     }
     return "valid"
   }
+
   /** Called when the call-hierarchy tab is opened */
   constructor({ providerRegistry }: { providerRegistry: ProviderRegistry<CallHierarchyProvider> }) {
     super()
@@ -72,12 +82,14 @@ export class CallHierarchyView extends HTMLElement {
       })
     )
   }
+
   /** Toogle between incoming and outgoing displays */
   #toggleCurrentType = () => {
     this.#currentType = this.#currentType === "incoming" ? "outgoing" : "incoming"
     this.setAttribute("current-type", this.#currentType)
     this.showCallHierarchy()
   }
+
   /** Show call hierarchy for {editor} and {point} */
   async showCallHierarchy(editor?: TextEditor, point?: Point) {
     if (this.destroyed) {
@@ -103,6 +115,7 @@ export class CallHierarchyView extends HTMLElement {
         : provider.getOutgoingCallHierarchy(targetEditor, targetPoint))
     )
   }
+
   /** Show call hierarchy for {newData} */
   #updateCallHierarchyView = async (newData: CallHierarchy<CallHierarchyType> | statusKey | null | undefined) => {
     const prevStatus = this.#status
@@ -126,6 +139,7 @@ export class CallHierarchyView extends HTMLElement {
     const item = new CallHierarchyViewStatusItem(statuses[currentStatus])
     this.#outputElement.appendChild(item)
   }
+
   /** Called when the call-hierarchy tab is closed */
   destroy() {
     this.innerHTML = ""
@@ -138,12 +152,16 @@ customElements.define("atom-ide-outline-call-hierarchy-view", CallHierarchyView)
 
 /** HTMLElement for the call-hierarchy item */
 class CallHierarchyViewItem<T extends CallHierarchyType> extends HTMLElement {
+
   #callHierarchy: CallHierarchy<T> | undefined
+
   #dblclickWaitTime = 300
+
   /** Whether {callHierarchy} data is undefined or empty array */
   static isEmpty(callHierarchy: CallHierarchy<CallHierarchyType> | undefined): callHierarchy is undefined {
     return !callHierarchy || callHierarchy.data.length == 0
   }
+
   constructor(callHierarchy: CallHierarchy<T> | undefined) {
     super()
     this.#callHierarchy = callHierarchy
@@ -190,6 +208,7 @@ class CallHierarchyViewItem<T extends CallHierarchyType> extends HTMLElement {
       })
     )
   }
+  
   /** Toggle the display of the {i}-th item */
   async toggleItemAt(i: number) {
     const itemEl = this.querySelectorAll<HTMLLIElement>(":scope>div")[i]
@@ -211,11 +230,13 @@ class CallHierarchyViewItem<T extends CallHierarchyType> extends HTMLElement {
       titleEl?.classList.replace("icon-chevron-right", "icon-chevron-down")
     }
   }
+
   /** Toggle the display of all item */
   async toggleAllItem() {
     const dataLen = this.#callHierarchy?.data.length ?? 0
     await Promise.all([...Array(dataLen).keys()].map((i) => this.toggleItemAt(i)))
   }
+
   /** Show document for {range} and {path}, and select {selectionRange} */
   #showDocument = ({
     path,
